f9c4c246842fd417e79d417f0b692de53f2d96ee
Vyacheslav Selivanov <vus1@mail.ru>
2022-05-23 16:04:34 +0700

    Change PurchaseItemRcAdapter

--- app/src/main/java/com/vk/vsvans/BlogShop/fragments/PurchaseItemListFragment.kt
+++ app/src/main/java/com/vk/vsvans/BlogShop/fragments/PurchaseItemListFragment.kt
@@ -11,14 +11,13 @@ import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.vk.vsvans.BlogShop.activity.EditPurchaseActivity
 
 import com.vk.vsvans.BlogShop.R
 import com.vk.vsvans.BlogShop.adapters.PurchaseItemRcAdapter
+import com.vk.vsvans.BlogShop.databinding.ListPurchaseItemFragBinding
 import com.vk.vsvans.BlogShop.dialogs.DialogHelper
 import com.vk.vsvans.BlogShop.interfaces.*
-import com.vk.vsvans.BlogShop.model.Product
 
 import com.vk.vsvans.BlogShop.model.PurchaseItem
 
@@ -27,36 +26,33 @@ import kotlinx.coroutines.Job
 //!реклама
 //class PurchaseItemListFragment(private val fragCloseInterface: FragmentCloseInterface, private val newList:ArrayList<PurchaseItem>?) : BaseMobAdFrag(),
 //    AdapterCallback {
-class PurchaseItemListFragment(private val fragCloseInterface:FragmentCloseInterface,val fragCallBack: IFragmentCallBack,private val newList:ArrayList<PurchaseItem>?) : Fragment(),AdapterCallback {
+class PurchaseItemListFragment(private val fragCloseInterface:FragmentCloseInterface,val fragCallBack: IFragmentCallBack,private val newList:ArrayList<PurchaseItem>?) : Fragment() {
 
-    //lateinit var rootElement:ListImageFragBinding
+    //    lateinit var rootElement: ListPurchaseItemFragBinding
     lateinit var adapter: PurchaseItemRcAdapter
-//    val dragCallback= ItemTouchMoveCallBack(adapter)
+    //    val dragCallback= ItemTouchMoveCallBack(adapter)
 //    val touchHelper=ItemTouchHelper(dragCallback)
     private var job: Job?=null
     //private var addPurchaseItem:MenuItem?=null
-    private lateinit var tb:Toolbar
+    //private lateinit var tb:Toolbar
     val TAG="MyLog"
     private var selectedId=0
-//    lateinit var binding:ListProductFragBinding
+    lateinit var binding:ListPurchaseItemFragBinding
 
-//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        binding= ListProductFragBinding.inflate(layoutInflater)
-//
-////!реклама
-////        adView=binding.adView
-//
-//        return binding.root
-//    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-      // super.onCreateView(inflater, container, savedInstanceState)
-    val view: View = inflater.inflate(R.layout.list_purchase_item_frag, container,false)
-    return view
-}
+        binding= ListPurchaseItemFragBinding.inflate(layoutInflater)
+
+//!реклама
+//        adView=binding.adView
+
+        return binding.root
+    }
+
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
+        //Assign adapter
         adapter= PurchaseItemRcAdapter(object: OnClickItemCallback {
             override fun onClickItem(id:Int) {
                 if(id>0) {
@@ -69,14 +65,12 @@ class PurchaseItemListFragment(private val fragCloseInterface:FragmentCloseInter
                 val pit= adapter.getPurchaseItem()
                 if (pit != null) {
                     DialogHelper.showPurchaseItemInputDialog(activity as EditPurchaseActivity,pit,
-                    object:IUpdatePurchaseItemList{
-                        override fun onUpdatePurchaseItemList(pit: PurchaseItem) {
-                           // adapter.setPurchaseItem(pit, adapter.selected_position)
-                            adapter.updateAdapterEdit(pit)
-                            //(activity as EditPurchaseActivity).dbManager.updatePurchaseItem(pit)
-                        }
+                        object:IUpdatePurchaseItemList{
+                            override fun onUpdatePurchaseItemList(pit: PurchaseItem) {
+                                adapter.updateAdapterEdit(pit)
+                            }
 
-                    })
+                        })
                 }
             }
 
@@ -94,41 +88,28 @@ class PurchaseItemListFragment(private val fragCloseInterface:FragmentCloseInter
                 }
             }
         })
+// end assign adapter
+
         val dragCallback= ItemTouchMoveCallBack(adapter)
         val touchHelper=ItemTouchHelper(dragCallback)
-        //устанавливаем тоолбар
-        tb=view.findViewById<Toolbar>(R.id.tb)
-        setUpToolbar()
-        val rcView=view.findViewById<RecyclerView>(R.id.rcViewSelectPurchaseItem)
-        touchHelper.attachToRecyclerView(rcView)
 
-        rcView.layoutManager = LinearLayoutManager(activity)
-        rcView.adapter = adapter
+        binding.apply {
+            //val rcView=view.findViewById<RecyclerView>(R.id.rcViewSelectImage)
+            setupToolbar()
+            val rcView = rcViewSelectPurchaseItem
+            touchHelper.attachToRecyclerView(rcView)
+            rcView.layoutManager = LinearLayoutManager(activity)
+            rcView.adapter = adapter
 
-        if (newList != null) {
-            adapter.updateAdapter(newList)
+            if (newList != null) {
+                adapter.updateAdapter(newList)
+            }
 
         }
-//        binding.apply {
-//            //val rcView=view.findViewById<RecyclerView>(R.id.rcViewSelectImage)
-//            val rcView = rcViewSelectPurchaseItem
-////            touchHelper.attachToRecyclerView(rcView)
-//            rcView.layoutManager = LinearLayoutManager(activity)
-//
-//            rcView.adapter = adapter
-//
-//            if (newList != null) {
-//                //adapter.updateAdapter(newList,true)
-//                adapter.update(newList)
-//            }
-//
-//        }
-
     }
 
     override fun onResume() {
         super.onResume()
-        setAddImageButton()
     }
 
     override fun onDetach() {
@@ -138,39 +119,12 @@ class PurchaseItemListFragment(private val fragCloseInterface:FragmentCloseInter
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    private fun setUpToolbar(){
-//        binding.apply {
+    private fun setupToolbar(){
+        // кнопка home <- слушатель
+        binding.tb.setNavigationOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()?.remove(this@PurchaseItemListFragment)?.commit()
+        }
 
-            //tb.inflateMenu(R.menu.menu_choose_purchase_item)
-            // кнопка home <- слушатель
-            tb.setNavigationOnClickListener {
-                // просто this будет ссылаться на binding класс поэтому this@ImageListFrag
-                //!реклама
-                activity?.supportFragmentManager?.beginTransaction()?.remove(this@PurchaseItemListFragment)?.commit()
-                //!реклама
-                //            showInterAd()
-            }
-
-    }
-
-
-    fun setAddImageButton(){
-//        if(adapter.mainArray.size <ImagePicker.MAX_IMAGE_COUNT){
-//            addImageItem?.isVisible=true
-//        }else{
-//            addImageItem?.isVisible=false
-//        }
-    }
-
-    // вызывается из фрагмента при очистке списка выбранных картинок
-
-
-    override fun onItemDelete(pit:PurchaseItem) {
-        if(fragCallBack!=null) fragCallBack.onFragmentCallBack(pit)
-//        if(activity!=null){
-//            (activity as EditPurchaseActivity).deletePurchaseItem(pit)
-//        }
-        //setAddImageButton()
     }
 
 //!реклама
