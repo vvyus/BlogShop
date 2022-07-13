2cbf8109b915f9da04726512502361bddebe8e1a
Vyacheslav Selivanov <vus1@mail.ru>
2022-07-13 13:28:56 +0700

    Добавил класс BaseAmount

---
+++ app/src/main/java/com/vk/vsvans/BlogShop/model/data/BaseAmount.kt
@@ -0,0 +1,7 @@
+package com.vk.vsvans.BlogShop.model.data
+
+open class BaseAmount:BaseList() {
+    var yearAmount=0.0
+    var monthAmount=0.0
+    var weekAmount=0.0
+}
--- app/src/main/java/com/vk/vsvans/BlogShop/model/data/ProductAmount.kt
+++ app/src/main/java/com/vk/vsvans/BlogShop/model/data/ProductAmount.kt
@@ -1,7 +1,4 @@
 package com.vk.vsvans.BlogShop.model.data
 
-class ProductAmount:Product() {
-    var yearAmount=0.0
-    var monthAmount=0.0
-    var weekAmount=0.0
+class ProductAmount:BaseAmount() {
 }
